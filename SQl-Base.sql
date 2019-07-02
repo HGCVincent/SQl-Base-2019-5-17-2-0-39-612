95fed186b1ea77020403160ac4aa9bab1ed81177
Vincent <1061271330@qq.com>
2019-07-03 01:11:15 +0800

    Vincent's commit

---
+++ SQl-Base.sql
@@ -0,0 +1,25 @@
+﻿create database student_examination_sys;
+
+use student_examination_sys;
+create table student(
+    id varchar(10) primary key,
+    name varchar(50),
+    age int,
+    sex varchar(10)
+);
+
+create table subject(
+    id varchar(10) PRIMARY key,
+    subject varchar(50),
+    teacher varchar(50),
+    description varchar(200)
+);
+
+create table score(
+    id varchar(10) PRIMARY key,
+    student_id varchar(10),
+    subject_id varchar(10),
+    score int,
+    FOREIGN key(student_id) REFERENCES student(id),
+    FOREIGN key(subject_id) REFERENCES subject(id)
+);
